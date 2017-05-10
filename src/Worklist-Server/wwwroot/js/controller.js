9ca3ac5e6217159acfe3050e7585fdc430b02e04
Matthias Schaffer <schaffer.matthias@gmail.com>
2017-05-10 21:39:59 +0200

    added angular and bootstrap, default site now show shows a list of all items

---
+++ src/Worklist-Server/wwwroot/js/controller.js
@@ -0,0 +1,24 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('WorklistApp')
+        .controller('WorklistController', controller1);
+
+    controller1.$inject = ['$scope', '$http'];
+
+    function controller1($scope, $http) {
+        var promise = $http.get("/api/worklistitems");
+        promise.then(function(res) {
+            return $scope.items = res.data;
+        }, function(err) {
+            $scope.errorMsg = 'could not load services';
+            console.log("err: ");
+            return console.log(err);
+        });
+
+        activate();
+
+        function activate() { }
+    }
+})();
